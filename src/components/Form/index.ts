e2990fb6424138a7da78a8c530bb635a6ccffdc4
Brandon Jernigan <brandon.jernigan@flosports.tv>
2025-12-29 19:57:24 -0600

    refactor(form): update exports for new API

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,42 +1,14 @@
 export { Form } from './Form';
-export { Field } from './Field';
-export { FieldArray } from './FieldArray';
 export { FormActions } from './FormActions';
 export { FormMessage } from './FormMessage';
-export { FormStepper } from './FormStepper';
-export { FormStep } from './FormStep';
-export { useFormContext } from './FormContext';
-export { useFormStep } from './hooks/useFormStep';
 
+// Export types
 export type {
   FormProps,
-  FormContextValue,
-} from './Form.types';
-
-export type {
-  FieldProps,
+  FormHandle,
+  FormRenderProps,
   FieldComponent,
-} from './Field.types';
-
-export type {
-  FieldArrayProps,
-} from './FieldArray.types';
-
-export type {
-  FormActionsProps,
-  FormActionsAlignment,
-} from './FormActions.types';
-
-export type {
-  FormMessageProps,
-  FormMessageType,
-} from './FormMessage.types';
-
-export type {
-  FormStepperProps,
-  FormStepperContextValue,
-} from './FormStepper.types';
-
-export type {
-  FormStepProps,
-} from './FormStep.types';
+  FieldProps,
+} from './Form.types';
+export type { FormActionsProps } from './FormActions.types';
+export type { FormMessageProps } from './FormMessage.types';
